ce7099edaf58beb083afd279f82030e2934a23d8
wa1tnr <wa1tnr@users.noreply.github.com>
2019-08-25 12:02:15 +0000

    Cortex-Forth version of this file

--- src/periph/dotstar.cpp
+++ src/periph/dotstar.cpp
@@ -1,44 +1,90 @@
-// Mon Jan 15 18:14:33 UTC 2018
-// 4737-a0d-05c-
-
-// version bump
-
-// Sat Jul 29 18:14:02 UTC 2017
-// 4735-b0b-01-
-
+// Sun Jun  9 01:26:44 UTC 2019
+#include "compatibility.h"
+#ifdef HAS_DOTSTAR_LIB
+#include <Arduino.h>
 #include <Adafruit_DotStar.h>
-
 #define NUMPIXELS 1 // Number of LEDs in strip
 
-#define DATAPIN    3
-#define CLOCKPIN   4
+/*
+
+dotstar pins
+
+  ItsyBitsy:
+  PB02 clock D6
+  PB03 data  D8
+
+  Trinket Mo:
+  PA01 clock D8
+  PA00 data D7
+
+*/
+
+#ifdef ADAFRUIT_GEMMA_M0
+
+  #undef DATAPIN
+  #undef CLOCKPIN
+
+  #define DATAPIN    3
+  #define CLOCKPIN   4
+
+#endif // #ifdef ADAFRUIT_GEMMA_M0
+
+#ifdef ADAFRUIT_TRINKET_M0
+
+  #undef DATAPIN
+  #undef CLOCKPIN
+
+  #define DATAPIN    7
+  #define CLOCKPIN   8
+
+#endif // #ifdef ADAFRUIT_TRINKET_M0
+
+#ifdef ADAFRUIT_ITSYBITSY_M4_EXPRESS
+
+  #undef DATAPIN
+  #undef CLOCKPIN
+
+  #define DATAPIN    8
+  #define CLOCKPIN   6
+
+#endif // #ifdef ADAFRUIT_ITSYBITSY_M4_EXPRESS
 
 Adafruit_DotStar strip = Adafruit_DotStar(
   NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
 
-void setup_dotstar() {
+void setup_dotstar(void) {
   strip.begin(); // Initialize pins for output
   strip.show();  // Turn all LEDs off ASAP
 }
 
-int      head  = 0, tail = -3;  // Index of first 'on' and 'off' pixels
-uint32_t color = 0x010000;      // 'On' color (starts red)
+uint16_t red = 0, green = 0, blue = 0;
 
-void loop_dotstar() {
-  strip.setPixelColor(head, color); // 'On' pixel at head
-  strip.setPixelColor(tail, 0);     // 'Off' pixel at tail
-  strip.show();                     // Refresh strip
-  delay(20);                        // Pause 20 milliseconds (~50 FPS)
-
-  if(++head >= NUMPIXELS) {         // Increment head index.  Off end of strip?
-    head = 0;                       //  Yes, reset head index to start
-    if((color >>= 8) == 0)          //  Next color (R->G->B) ... past blue now?
-      color = 0x010000;             //   Yes, reset to red
-  }
-  if(++tail >= NUMPIXELS) tail = 0; // Increment, reset tail index
-
-Serial.println("\r\nWent off tail.  Bye from dotstar.");
-
+void clear_dotStarColors(void) {
+  red = 0; green = 0; blue = 0; // black/no colors/no light at all
+  strip.setPixelColor(0, blue, red, green); // Set the pixel colors
+  strip.show();              // Refresh strip
 }
 
-// END
+void set_dotStarColors(void) {
+  red = 0; green = 22; blue = 22; // cyan
+  strip.setPixelColor(0, blue, red, green);
+  strip.show();
+}
+
+void wiggleDotStarOnce(void) { // toggle at a rate
+  set_dotStarColors();
+  delay(200);
+  clear_dotStarColors();
+  delay(200);
+}
+
+void wiggleDSManyTimes(void) { // repeat the toggle
+  for (int i = 8; i>0; i--) {
+    wiggleDotStarOnce();
+  }
+}
+
+void loop_dotstar(void) { 
+  wiggleDotStarOnce();
+}
+#endif // HAS_DOTSTAR_LIB
